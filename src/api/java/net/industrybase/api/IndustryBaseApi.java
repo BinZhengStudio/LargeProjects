1953a718e2eba8b543516601faa9553ae15a138a
vvvbbbcz <vvvbbbcz@bzgzs.cn>
2024-12-17 22:52:00 +0800

    ?

--- src/api/java/net/industrybase/api/IndustryBaseApi.java
+++ src/api/java/net/industrybase/api/IndustryBaseApi.java
@@ -1,5 +1,9 @@
 package net.industrybase.api;
 
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
 public class IndustryBaseApi {
+	public static final Logger LOGGER = LogUtils.getLogger();
 	public static final String MODID = "industrybase";
 }
--- src/api/java/net/industrybase/api/util/NbtHelper.java
+++ src/api/java/net/industrybase/api/util/NbtHelper.java
@@ -1,14 +1,16 @@
 package net.industrybase.api.util;
 
+import net.industrybase.api.IndustryBaseApi;
 import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.IntArrayTag;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 public class NbtHelper {
 	public static Optional<BlockPos> readBlockPos(IntArrayTag tag) {
 		int[] array = tag.getAsIntArray();
+		if (array.length != 3) IndustryBaseApi.LOGGER.warn("Invalid BlockPos: {}", Arrays.toString(array));
 		return array.length == 3 ? Optional.of(new BlockPos(array[0], array[1], array[2])) : Optional.empty();
 	}
 }
